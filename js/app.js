fc83d22a0edc6f480ae914f34bf062a451edca07
Artem Leonov <artem_leonov@hotmail.com>
2021-11-27 19:35:17 +0200

    Done iss2

--- js/app.js
+++ js/app.js
@@ -5,3 +5,37 @@ function isNum(num) {
 function iss1() {
     alert(isNum(prompt('Введите текст или строку')));
 }
+
+function emptyLine (line) {
+    return !line;
+}
+
+function mean(num1, num2, num3) {
+    if (num1 === num2 && num2 === num3) {
+        return num1;
+    }
+    if (num1 > num2 && num1 < num3) {
+        return num1;
+    }
+    if (num1 < num2 && num1 > num3) {
+        return num1;
+    }
+    if (num2 > num1 && num2 < num3) {
+        return num2;
+    }
+    if (num2 < num1 && num2 > num3) {
+        return num2;
+    }
+    return num3;
+}
+
+function iss2() {
+    let num1 = prompt('Введите первое число');
+    let num2 = prompt('Введите второе число');
+    let num3 = prompt('Введите третье число');
+    if (isNum(num1) && !emptyLine(num1) && isNum(num2) && !emptyLine(num2) && isNum(num3) && !emptyLine(num3)) {
+        alert(mean(+num1, +num2, +num3));
+    } else {
+        alert('Есть поле с пустым или не числовым значением!')
+    }
+}
