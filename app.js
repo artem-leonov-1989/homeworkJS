3c69f64b1a8fec03154f78c528cd4bcc5a572596
Artem Leonov <artem_leonov@hotmail.com>
2021-12-03 17:54:11 +0200

    Done iss3

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ function iss1() {
 function iss2() {
     document.getElementsByName('inputTextIss2')[0].value = 'Hello, World!';
 }
+
+function iss3() {
+    console.log(document.getElementsByName('inputTextIss3')[0].value);
+}
