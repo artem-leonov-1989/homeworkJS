1c6c937884c01326ecb8e2537237d4a3ddc229f0
Artem Leonov <artem_leonov@hotmail.com>
2021-12-05 20:14:30 +0200

    Done iss10

--- app.js
+++ app.js
@@ -65,3 +65,59 @@ function iss9() {
         mainDiv.appendChild(div);
     }
 }
+
+function iss10() {
+    let arr = [];
+    let formObj = [];
+    let userName = document.getElementById('userName');
+    formObj[0] = userName;
+    if (userName.value.length < 4 || userName.value.length > 20) {
+        arr[0] = false;
+    } else {
+        if (userName.value.search('_') !== -1 || userName.value.search('/') !== -1 || userName.value.search(/\\/) !== -1 || userName.value.search(/\|/) !== -1) {
+            arr[0] = false;
+        }
+        arr[0] = true;
+    }
+    let userEmail = document.getElementById('userEmail');
+    formObj[1] = userEmail;
+    arr[1] = !!(userEmail.value && userEmail.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/));
+    let userAge = document.getElementById('userAge');
+    formObj[2] = userAge;
+    if (userAge.value) {
+        arr[2] = !isNaN(Number(userAge.value)) && Number(userAge.value) >= 0;
+    } else {
+        arr[2] = false;
+    }
+    let pass = document.getElementById('pass');
+    formObj[3] = pass;
+    if (pass.value.length > 5) {
+        arr[3] = pass.value.search(/\d+/) >= 0 && pass.value.search(/[A-Z]/) >= 0;
+    } else {
+        arr[3] = false;
+    }
+    let repPass = document.getElementById('repPass');
+    formObj[4] = repPass;
+    arr[4] = repPass.value === pass.value;
+    if (arr[0] === true && arr[1] === true && arr[2] === true && arr[3] === true && arr[4] === true) {
+        for (let i = 0; i < formObj.length; i++) {
+            changeProperties(formObj[i], false);
+            formObj[i].value = '';
+        }
+        document.getElementById('successfully').style.display = 'block';
+    } else {
+        for (let i = 0; i < formObj.length; i++) {
+            changeProperties(formObj[i], !arr[i]);
+        }
+    }
+}
+
+function changeProperties (id, show) {
+    if (show) {
+        id.style.borderColor = 'red';
+        document.querySelector(`[for = ${id.id}]`).style.display = 'block';
+    } else {
+        id.style.borderColor = 'unset';
+        document.querySelector(`[for = ${id.id}]`).style.display = 'none';
+    }
+}
