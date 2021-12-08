e49d570e7b1b083356d2581ef5f009d13b6dc50c
Artem Leonov <artem_leonov@hotmail.com>
2021-12-08 15:23:11 +0200

    Модификация модального окна

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-function showModalWindow () {
-    let modalWindow = document.getElementsByClassName('modal')[0];
-    modalWindow.style.display = 'block';
+function showObj (Obj) {
+    Obj.style.display = 'block';
+}
+
+function putAwayObj (Obj) {
+    Obj.style.display = 'none';
+}
+
+function createModalWindow() {
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    let modalWindow = document.createElement('div');
+    modalWindow.classList.add('modalWindow');
+    let modalClose = document.createElement('div');
+    modalClose.classList.add('modalClose');
+    modalClose.classList.add('flex');
+    let closeButton = document.createElement('button');
+    closeButton.type = 'button';
+    closeButton.innerText = 'X';
+    closeButton.addEventListener('click', function () {
+        document.getElementsByClassName('modal')[0].remove();
+    })
+    modalClose.appendChild(closeButton);
+    modalWindow.appendChild(modalClose);
+
+    modal.appendChild(modalWindow);
+    return modal;
+}
+
+function addObj(Obj) {
+    document.body.appendChild(Obj);
+}
+
+function showModalWindow() {
+    let window = createModalWindow()
+    addObj(window);
 }
